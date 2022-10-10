d6921cd4bb60ea03a19953eb2895ce22c6879e36
aurora <aurora@wih.lgbt>
2022-10-10 20:26:55 +0800

    feat: add prettier

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="my-5 text-center">FutureX Frontend App</div>
 </template>
 
-<style scoped lang="less">
-
-</style>
+<style scoped lang="less"></style>
